8a6c62b03b42aaa5c38ad359476ff2e51fdaf20b
iameasyguy <thebotsguy@gmail.com>
2019-12-27 22:45:19 +0300

    updated area location

--- resources/views/modules/reports/create.blade.php
+++ resources/views/modules/reports/create.blade.php
@@ -96,10 +96,6 @@
                                         </div>
                                     </div>
                                 </div>
-                                {{--                                ['project_id','installer','inspected_by','personnel','t_fabrication','t_profile','no_screws',--}}
-                                {{--                                't_erection','t_spacing','t_align','t_anchor','c_details','b_details','brc_bcb','wr_wb',--}}
-                                {{--                                'tcb','w_stiffener','w_beam','t_brace','p_fascia','p_spacing','f_fixing','f_alignment',--}}
-                                {{--                                'r_cover','c_type','f_spacing','v_ridges','w_flashing','s_touch','comments','prepared_by','confirmed_by','status'];--}}
 
                                 <div class="row">
 
